460eeec2df10f4a3237e2edf46249c39af342c75
struwec <118317872+struwec@users.noreply.github.com>
2023-04-26 10:31:37 -0500

    Create extract_GQL_IP.py

---
+++ AnsibleCore/GQL_Inv/extract_GQL_IP.py
@@ -0,0 +1,19 @@
+import json
+
+#Opens the JSON file with the output from the dynamic-inventory and loads it into data
+with open('gql_Inventory.json') as f:
+    data = json.load(f)
+
+inventory_output = ''
+
+for device in data['json']['data']['devices']:
+    name = device['name']
+    ip_address = device['primary_ip4']['address'].split('/')[0]
+    inventory_output += name + ' '
+    inventory_output += 'ansible_host=' + ip_address + ' '
+    inventory_output += 'ansible_network_os=eos\n'
+
+with open('gql_inventory.ini', 'w') as f:
+    f.write(inventory_output)
+
+
